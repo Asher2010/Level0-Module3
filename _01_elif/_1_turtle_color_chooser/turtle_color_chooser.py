e7de639a2c05d97dbab2905d6173fe22fb039ac3
league <asherwelsbie@icloud.com>
2023-10-15 12:57:17 -0700

    cats, color square

--- _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
+++ _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
@@ -15,12 +15,33 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # TODO 1) Create a new Turtle
+    Turty = turtle.Turtle()
     #      2) Make the turtle draw a shape (this will take more than one line
     #         of code)
+    for i in range(6):
+        Turty.forward(200)
+        Turty.left(90)
+        Turty.forward(200)
+        Turty.left(90)
+        Turty.forward(200)
+        Turty.left(90)
+        Turty.forward(200)
     #      3) Set the pen width to 10
+
+        Turty.pensize(10)
     #      4) Ask the user what color pen they would like to draw with
+        Color = simpledialog.askstring(title="Color", prompt="What color pen would you like to draw with?")
     #      5) Use an if/else statement to set the pen color that the user
     #         requested
+        if Color == "blue":
+            Turty.pencolor("blue")
+        elif Color == "red":
+            Turty.pencolor("red")
+        elif Color == "green":
+            Turty.pencolor("green")
+        else:
+            Turty.pencolor(get_random_color())
+
     #      6) If the user doesn't enter anything, choose a random color
     #      7) Put a loop around your code so that you keep asking the user for
     #         more colors & drawing them
